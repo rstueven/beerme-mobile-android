022602f19805cd23687233e8a5a17b5a9dae8c01
rstueven <richard@igroupstudios.com>
2017-07-09 19:57:08 -0500

    Application colors

--- beerme/src/main/java/com/beerme/android/MainActivity.java
+++ beerme/src/main/java/com/beerme/android/MainActivity.java
@@ -11,7 +11,6 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.text.TextUtils;
-import android.util.Log;
 import android.util.SparseArray;
 import android.view.LayoutInflater;
 import android.view.Menu;

838c42f276836a123ead851d6eb514947b340451
rstueven <richard@igroupstudios.com>
2017-07-19 20:08:07 -0500

    Implement brewery search results as a Fragment

---
+++ beerme/src/main/java/com/beerme/android/BreweryListFragment.java
@@ -0,0 +1,122 @@
+package com.beerme.android;
+
+import android.app.Fragment;
+import android.app.LoaderManager;
+import android.content.Context;
+import android.content.CursorLoader;
+import android.content.Intent;
+import android.content.Loader;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import com.beerme.android.db.DBContract;
+
+public class BreweryListFragment extends Fragment
+        implements LoaderManager.LoaderCallbacks<Cursor>, SearchActivity.BeerMeSearch {
+    private final int BREWERY_LOADER = 0;
+    private SearchAdapter breweryAdapter;
+    private Context mContext;
+
+    public BreweryListFragment() {
+    }
+
+    public static BreweryListFragment newInstance() {
+        final BreweryListFragment fragment = new BreweryListFragment();
+        final Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(final LayoutInflater inflater, final ViewGroup container, final Bundle savedInstanceState) {
+        final View view = inflater.inflate(R.layout.fragment_brewery_list, container, false);
+        final ListView listView = (ListView) view.findViewById(R.id.brewery_list);
+        listView.setEmptyView(view.findViewById(R.id.brewery_list_empty));
+        breweryAdapter = new SearchAdapter(mContext, null);
+        listView.setAdapter(breweryAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(final AdapterView<?> parent, final View view, final int position, final long id) {
+                final Intent intent = new Intent(mContext, BreweryActivity.class);
+                intent.putExtra("id", (int) id);
+                startActivity(intent);
+            }
+        });
+
+        return view;
+    }
+
+    @Override
+    public void onAttach(final Context context) {
+        super.onAttach(context);
+        mContext = context;
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+
+    @Override
+    public void search(final String query) {
+        final String[] projection = new String[]{DBContract.Brewery.COLUMN_ID, DBContract.Brewery.COLUMN_NAME, DBContract.Brewery.COLUMN_ADDRESS};
+        final Bundle breweryArgs = new Bundle();
+        breweryArgs.putStringArray("selection_args", new String[]{"%" + query + "%"});
+        breweryArgs.putStringArray("projection", projection);
+        getLoaderManager().initLoader(BREWERY_LOADER, breweryArgs, this);
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(final int id, final Bundle args) {
+        final String SELECTION = "name LIKE ?";
+        final String ORDER = "name ASC";
+
+        switch (id) {
+            case BREWERY_LOADER:
+                return new CursorLoader(mContext,
+                        DBContract.Brewery.CONTENT_URI,
+                        args.getStringArray("projection"),
+                        SELECTION,
+                        args.getStringArray("selection_args"),
+                        ORDER);
+            default:
+                throw new IllegalArgumentException("Unknown id: " + id);
+        }
+    }
+
+    @Override
+    public void onLoadFinished(final Loader<Cursor> loader, final Cursor data) {
+        final int id = loader.getId();
+
+        switch (id) {
+            case BREWERY_LOADER:
+                breweryAdapter.swapCursor(data);
+                break;
+            default:
+                throw new IllegalArgumentException("Unknown id: " + id);
+        }
+    }
+
+    @Override
+    public void onLoaderReset(final Loader<Cursor> loader) {
+        final int id = loader.getId();
+
+        switch (id) {
+            case BREWERY_LOADER:
+                breweryAdapter.swapCursor(null);
+                break;
+            default:
+                throw new IllegalArgumentException("Unknown id: " + id);
+        }
+    }
+}
--- beerme/src/main/java/com/beerme/android/SearchActivity.java
+++ beerme/src/main/java/com/beerme/android/SearchActivity.java
@@ -1,5 +1,7 @@
 package com.beerme.android;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
 import android.app.LoaderManager;
 import android.app.SearchManager;
 import android.content.CursorLoader;
@@ -20,18 +22,20 @@ import com.beerme.android.db.DBContract;
  * Handles searches.
  */
 
-public class SearchActivity extends BeerMeActivity
-        implements LoaderManager.LoaderCallbacks<Cursor> {
-    private final int BREWERY_LOADER = 0;
-    private final int BEER_LOADER = 1;
-    private final int LOCATION_LOADER = 2;
+public class SearchActivity extends BeerMeActivity {
+    BreweryListFragment breweryFrag;
 
-    private SearchAdapter breweryAdapter;
+    interface BeerMeSearch {
+        void search(String query);
+    }
 
     @Override
     public void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+        final FragmentManager fm = getFragmentManager();
+        breweryFrag = (BreweryListFragment) fm.findFragmentById(R.id.brewery_list_fragment);
 
         final Intent intent = getIntent();
         if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
@@ -42,89 +46,9 @@ public class SearchActivity extends BeerMeActivity
 
     private void search(final String query) {
         if (!TextUtils.isEmpty(query)) {
-            searchBreweries(query);
-//            searchBeers(query);
-//            searchLocations(query);
+            breweryFrag.search(query);
         } else {
             this.finish();
-        }
-    }
-
-    private void searchBreweries(final String query) {
-        final String[] projection = new String[]{DBContract.Brewery.COLUMN_ID, DBContract.Brewery.COLUMN_NAME, DBContract.Brewery.COLUMN_ADDRESS};
-        final Bundle breweryArgs = new Bundle();
-        breweryArgs.putStringArray("selection_args", new String[]{"%" + query + "%"});
-        breweryArgs.putStringArray("projection", projection);
-        final ListView listView = (ListView) findViewById(R.id.brewery_list);
-        listView.setEmptyView(findViewById(R.id.brewery_list_empty));
-        breweryAdapter = new SearchAdapter(this, null);
-        listView.setAdapter(breweryAdapter);
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(final AdapterView<?> parent, final View view, final int position, final long id) {
-                final Intent intent = new Intent(SearchActivity.this, BreweryActivity.class);
-                intent.putExtra("id", (int) id);
-                startActivity(intent);
-            }
-        });
-        getLoaderManager().initLoader(BREWERY_LOADER, breweryArgs, this);
-    }
-
-    @Override
-    public Loader<Cursor> onCreateLoader(final int id, final Bundle args) {
-        final String SELECTION = "name LIKE ?";
-        final String ORDER = "name ASC";
-
-        switch (id) {
-            case BREWERY_LOADER:
-                return new CursorLoader(this,
-                        DBContract.Brewery.CONTENT_URI,
-                        args.getStringArray("projection"),
-                        SELECTION,
-                        args.getStringArray("selection_args"),
-                        ORDER);
-            case BEER_LOADER:
-                break;
-            case LOCATION_LOADER:
-                break;
-            default:
-                throw new IllegalArgumentException("Unknown id: " + id);
-        }
-
-        return null;
-    }
-
-    @Override
-    public void onLoadFinished(final Loader<Cursor> loader, final Cursor data) {
-        final int id = loader.getId();
-
-        switch (id) {
-            case BREWERY_LOADER:
-                breweryAdapter.swapCursor(data);
-                break;
-            case BEER_LOADER:
-                break;
-            case LOCATION_LOADER:
-                break;
-            default:
-                throw new IllegalArgumentException("Unknown id: " + id);
-        }
-    }
-
-    @Override
-    public void onLoaderReset(final Loader<Cursor> loader) {
-        final int id = loader.getId();
-
-        switch (id) {
-            case BREWERY_LOADER:
-                breweryAdapter.swapCursor(null);
-                break;
-            case BEER_LOADER:
-                break;
-            case LOCATION_LOADER:
-                break;
-            default:
-                throw new IllegalArgumentException("Unknown id: " + id);
         }
     }
 }

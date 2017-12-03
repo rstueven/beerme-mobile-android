9e4adf0ba0b38b8f84adb9087487a606d1171536
rstueven <richard@igroupstudios.com>
2017-12-03 16:36:48 -0600

    Remove redundant casts

--- beerme/src/main/java/com/beerme/android/EditBreweryActivity.java
+++ beerme/src/main/java/com/beerme/android/EditBreweryActivity.java
@@ -29,20 +29,20 @@ public class EditBreweryActivity extends BeerMeActivity {
             brewery = new Brewery(this, breweryId);
         }
 
-        final TextView titleView = (TextView) findViewById(R.id.title);
+        final TextView titleView = findViewById(R.id.title);
         titleView.setText(R.string.add_a_brewery);
-        final EditText nameView = (EditText) findViewById(R.id.name_view);
-        final EditText addressView = (EditText) findViewById(R.id.address_view);
+        final EditText nameView = findViewById(R.id.name_view);
+        final EditText addressView = findViewById(R.id.address_view);
         // TODO: Split address into city + region + zip.
 //        final EditText cityView = (EditText) findViewById(R.id.city_view);
 //        final AutoCompleteEditText regionView = (AutoCompleteEditText) findViewById(R.id.region_view);
 //        final ArrayAdapter<CharSequence> regionAdapter = ArrayAdapter.createFromResource(this, R.array.regions_array, android.R.layout.simple_spinner_item);
 //        regionView.setAdapter(regionAdapter);
 //        final EditText zipView = (EditText) findViewById(R.id.zip_view);
-        final EditText latView = (EditText) findViewById(R.id.lat_view);
-        final EditText lngView = (EditText) findViewById(R.id.lng_view);
-        final EditText phoneView = (EditText) findViewById(R.id.phone_view);
-        final Spinner statusView = (Spinner) findViewById(R.id.status_view);
+        final EditText latView = findViewById(R.id.lat_view);
+        final EditText lngView = findViewById(R.id.lng_view);
+        final EditText phoneView = findViewById(R.id.phone_view);
+        final Spinner statusView = findViewById(R.id.status_view);
         final Status.StatusAdapter statusAdapter = new Status.StatusAdapter(this);
         statusView.setAdapter(statusAdapter);
         // TODO: Maybe add these fields.
@@ -53,21 +53,21 @@ public class EditBreweryActivity extends BeerMeActivity {
 //        final ArrayAdapter<CharSequence> capUnitAdapter = ArrayAdapter.createFromResource(this, R.array.capacity_units_array, android.R.layout.simple_spinner_item);
 //        capUnitAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 //        capUnitView.setAdapter(capUnitAdapter);
-        final EditText webView = (EditText) findViewById(R.id.web_view);
+        final EditText webView = findViewById(R.id.web_view);
         // TODO: And this one.
 //        final EditText emailView = (EditText) findViewById(R.id.email_view);
-        final CheckBox publicView = (CheckBox) findViewById(R.id.public_view);
-        final EditText hoursView = (EditText) findViewById(R.id.hours_view);
-        final CheckBox barView = (CheckBox) findViewById(R.id.bar_view);
-        final CheckBox beergardenView = (CheckBox) findViewById(R.id.beergarden_view);
-        final CheckBox foodView = (CheckBox) findViewById(R.id.food_view);
-        final CheckBox giftshopView = (CheckBox) findViewById(R.id.giftshop_view);
-        final CheckBox hotelView = (CheckBox) findViewById(R.id.hotel_view);
-        final CheckBox retailView = (CheckBox) findViewById(R.id.retail_view);
-        final CheckBox toursView = (CheckBox) findViewById(R.id.tours_view);
-        final CheckBox wifiView = (CheckBox) findViewById(R.id.wifi_view);
-        final EditText commentsView = (EditText) findViewById(R.id.comments_view);
-        final Button submitBtn = (Button) findViewById(R.id.submit_btn);
+        final CheckBox publicView = findViewById(R.id.public_view);
+        final EditText hoursView = findViewById(R.id.hours_view);
+        final CheckBox barView = findViewById(R.id.bar_view);
+        final CheckBox beergardenView = findViewById(R.id.beergarden_view);
+        final CheckBox foodView = findViewById(R.id.food_view);
+        final CheckBox giftshopView = findViewById(R.id.giftshop_view);
+        final CheckBox hotelView = findViewById(R.id.hotel_view);
+        final CheckBox retailView = findViewById(R.id.retail_view);
+        final CheckBox toursView = findViewById(R.id.tours_view);
+        final CheckBox wifiView = findViewById(R.id.wifi_view);
+        final EditText commentsView = findViewById(R.id.comments_view);
+        final Button submitBtn = findViewById(R.id.submit_btn);
 
         if (brewery != null) {
             titleView.setText(R.string.edit_brewery_information);
